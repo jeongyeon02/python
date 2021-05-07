1f9d65fe99d8e08a438c21c42ac03b4b079479da
Yu Jeong Yeon <83645524+jeongyeon02@users.noreply.github.com>
2021-05-07 15:31:30 +0900

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,19 @@
+x = "apple"                   # 변수 x를 문자형 apple로 선언
+y = "banana"                  #변수 y를 문자형 banana로 선언
+z = """냉장고에 남은          # 변수 z를 문자형 냉장고에 남은 오렌지 하나로 2행 선언 (Shift + Alt + A)
+오렌지 하나"""                
+
+print(x)                      # 문자형 x 출력
+print(y)                      # 문자형 y 출력 
+print(z)                      # 문자형 z 출력
+print("\n")                   # 공백 행 
+print(x+y+z)                  # 문자형 x, y, z를 합산하여 출력
+
+출력값:
+apple
+banana
+냉장고에 남은
+오렌지 하나
+
+applebanana냉장고에 남은
+오렌지 하나
