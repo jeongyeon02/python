6218fbf5767e0d6d1f9d86fe8561615410e7e7d6
Yu Jeong Yeon <83645524+jeongyeon02@users.noreply.github.com>
2021-05-10 00:43:30 +0900

    Update 5-1.py

--- 5-1.py
+++ 5-1.py
@@ -6,11 +6,12 @@ y = False
 print(x), print(y)
 print("\n")
 
-a = 5
-b = 2
-if a > b :
+a = 5                                 # a를 숫자형 5로 선언
+b = 2                                 # b를 숫자형 2로 선언
+
+if a > b :                            # 만약 a가 b보아 크다면 a가 더 크다 출력
   print("a가 더 크다")
-print("True이기 때문에 출력 된다.")
+print("True이기 때문에 출력 된다.")   # 문자형True이기 때문에 출력 된다. 출력
 
 출력값:
 True
