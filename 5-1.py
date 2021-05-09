20eed56902bd29cd0e9fe810be9281217ad0b40d
Yu Jeong Yeon <83645524+jeongyeon02@users.noreply.github.com>
2021-05-10 00:26:39 +0900

    Create 5-1.py

---
+++ 5-1.py
@@ -0,0 +1,20 @@
+# 블리안(boolean) : 조건에 따라서
+
+x = True                             
+y = False
+
+print(x), print(y)
+print("\n")
+
+a = 5
+b = 2
+if a > b :
+  print("a가 더 크다")
+print("True이기 때문에 출력 된다.")
+
+출력값:
+True
+False
+
+a가 더 크다
+True이기 때문에 출력 된다
