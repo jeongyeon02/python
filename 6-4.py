ed423d8f215ff3b6384e12e2d82f768c9b0af97a
Yu Jeong Yeon <83645524+jeongyeon02@users.noreply.github.com>
2021-05-17 23:54:16 +0900

    Create 6-4.py

---
+++ 6-4.py
@@ -0,0 +1,25 @@
+#함수 4
+
+# def dsum(a, b):       
+#     result = a + b
+#     return result
+
+# c = dsum(8, 2)
+# print(c)
+
+# def dsum(a, b):
+#     result = a + b
+#     print(result)
+
+# c = dsum(8, 2)
+# print(c)
+
+def dsum(a, b):         # 지정된 함수 dsum에 a와 b를 파라미터로 받음
+    result = a + b      # a 와 b를 더한 것이 결과 
+    return result       # 결과 반화
+
+c = dsum(5, 2)          # 변수 c를 함수 dsum에 5와 2를 파라미터로 지정
+print(c)                # 변수 c 출력
+
+출력값:
+7  
