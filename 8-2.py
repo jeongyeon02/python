9f5d9bc084053bee430fb83ba2f3f00c94cb50c7
Yu Jeong Yeon <83645524+jeongyeon02@users.noreply.github.com>
2021-05-21 23:59:26 +0900

    Create 8-2.py

---
+++ 8-2.py
@@ -0,0 +1,16 @@
+x = [4,5,6,7,8,9,10]         # 변수 x의 리스트 설정
+num_elements = len(x)        # 변수 num_elementsx을 x 리스트의 길이로 선언 
+print(num_elements)          # 변수 num_elementsx 출력
+
+x = [5,100,85,79,31]         # 변수 x의 리스트 설정
+y = sorted(x)                # 변수 y를 x 리스트의 짧은순으로 선언
+print(y)                     # 변수 y 출력
+
+x = [10,100,1000,10000]      # 변수 x의 리스트 설정 
+y = sum(x)
+print(y)
+
+출력값:
+7
+[5, 31, 79, 85, 100]
+11110
