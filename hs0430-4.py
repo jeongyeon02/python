257a659e0b61e43854f784853b32bf778c0049a2
Yu Jeong Yeon <83645524+jeongyeon02@users.noreply.github.com>
2021-05-24 13:00:15 +0900

    Create hs0430-4.py

---
+++ hs0430-4.py
@@ -0,0 +1,18 @@
+# 환율 계산
+
+# 1)
+won = 1000                                        # 변수 won은 숫자형 2000000으로 선언
+usd = won * 1130                                  # 변수 usd는 won에 1130을 곱한 것으로 선언
+print(usd)                                        # usd 출력
+
+# 2)
+exchange_rate = int((input("현재 환율 입력: "))   # 변수 exchange_rate를 숫자형으로 입력하여 선언
+won = 1000                                        # 변수 won을 숫자형 2000000으로 선언
+usd = won * exchange_rate                         # 변수 usd는 won에 exchange_rate를 곱한 것으로 선언
+print(usd)                                        # usd 출력
+    
+입력값: 1130
+                    
+출력값:
+1130000
+1130000                    
