b8a431eafb799276f2dceb3348919508f031ee06
Yu Jeong Yeon <83645524+jeongyeon02@users.noreply.github.com>
2021-05-25 22:46:58 +0900

    Create hs0430-5.py

---
+++ hs0430-5.py
@@ -0,0 +1,22 @@
+# 화씨를 섭씨로 변환
+
+fahrenheit = 529
+celsius = fahrenheit - 32
+celsius = celsius * 5
+celsius = celsius / 9
+print(celsius)
+
+# 다수의 정수를 입력하여 합계 출력
+x = int(input("정수 x를 입력하세요: "))
+y = int(input("정수 y를 입력하세요: "))
+z = int(input("정수 z를 입력하세요: "))
+sum = x + y + z
+print(sum)
+      
+입력값: 
+5
+9
+3
+출력값:
+276.111111
+17       
