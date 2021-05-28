81bf4a26e873d68783deffc5d10faaad91909ba8
Yu Jeong Yeon <83645524+jeongyeon02@users.noreply.github.com>
2021-05-28 09:53:13 +0900

    Create hs0430-7.py

---
+++ hs0430-7.py
@@ -0,0 +1,26 @@
+# 배송료 계산 알고리즘
+
+price= int(input("상품의 가격을 입력하세요: "))
+if price > 50000:
+ shipping_cost = 0
+else:
+ shipping_cost = 3000
+print(shipping_cost)
+
+# 성적입력받아서 합격 유무 출력
+
+grade = int(input("성적을 입력하세요: "))
+if grade >= 80 :
+ print("합격입니다")
+else :
+ print("불합격입니다")
+
+입력값:
+54300
+92
+출력값:
+0
+합격입니다
+  
+  
+  
