217cedd96ed77a998dd764181eac056a6eec20c7
Yu Jeong Yeon <83645524+jeongyeon02@users.noreply.github.com>
2021-05-29 15:01:55 +0900

    Update hs0430-5.py

--- hs0430-5.py
+++ hs0430-5.py
@@ -1,17 +1,17 @@
 # 화씨를 섭씨로 변환
 
-fahrenheit = 529
-celsius = fahrenheit - 32
-celsius = celsius * 5
-celsius = celsius / 9
-print(celsius)
+fahrenheit = 529                         # 변수 fahrenheit를 정수 529로 선언
+celsius = fahrenheit - 32                # 변수 celsius를 변수 fahrenheit에 32를 뺀 것으로 선언 
+celsius = celsius * 5                    # 변수 celsius를 위의 값에서 5를 곱한 것으로 선언
+celsius = celsius / 9                    # 변수 celsius를 위의 값에서 9로 나눈 것으로 선언 
+print(celsius)                           # 변수 celsius 출력
 
 # 다수의 정수를 입력하여 합계 출력
-x = int(input("정수 x를 입력하세요: "))
-y = int(input("정수 y를 입력하세요: "))
-z = int(input("정수 z를 입력하세요: "))
-sum = x + y + z
-print(sum)
+x = int(input("정수 x를 입력하세요: "))  # 변수 x를 숫자형으로 선언하여 입력
+y = int(input("정수 y를 입력하세요: "))  # 변수 y를 숫자형으로 선언하여 입력
+z = int(input("정수 z를 입력하세요: "))  # 변수 z를 숫자형으로 선언하여 입력
+sum = x + y + z                         # 변수 sum을 변수 x ,y, z의 합으로 선언  
+print(sum)                              # 변수 sum 출력  
       
 입력값: 
 5
