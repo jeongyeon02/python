12643b271a857d08540cfc5ae4de736e9e924345
Yu Jeong Yeon <83645524+jeongyeon02@users.noreply.github.com>
2021-06-01 23:12:20 +0900

    Create hs0430-10.py

---
+++ hs0430-10.py
@@ -0,0 +1,31 @@
+# 시험 점수 계산하기
+
+phi = input('철학 점수를 입력하세요: ')
+eng = input('영어 점수를 입력하세요: ')
+log = input('논리 점수를 입력하세요: ')
+
+print("철학", phi)
+print("영어", eng)
+print("논리", log)
+
+phi = int(input('철학 점수를 입력하세요: '))
+eng = int(input('영어 점수를 입력하세요: '))
+log = int(input('논리 점수를 입력하세요: '))
+
+print('총점 : ', (phi + eng + log))
+print('평균 : ', (phi + eng + log) / 3)
+
+입력값:
+90
+85
+95
+90
+85
+95
+출력값:
+철학90
+영어85
+논리95
+총점:270
+평균:90.0  
+  
